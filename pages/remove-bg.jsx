a36774dcadfad6e099dca404f3ff38a1a927f02d
Nguyen Van Duc <ducbe2k2@gmail.com>
2022-06-01 23:55:51 +0700

    ghi chu van ban

---
+++ pages/remove-bg.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Layout from '../components/Layout';
+import axios from 'axios';
+import path from 'path';
+import FormData from 'form-data';
+import fs from 'fs';
+const RemoveBg = () => {
+
+  const fetchData = async () => {
+ 
+  }
+  React.useEffect(() => {
+    fetchData();
+  },[])
+  return (
+      <>
+      <Layout>
+
+      </Layout>
+      </>
+  )
+}
+
+export default RemoveBg
---
+++ pages/text-notes.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Layout from "../components/Layout";
+import TitlePage from "../components/PageComponents/TitlePage";
+
+const TextNotes = () => {
+  const [TextNotes,setTextNotes] = React.useState('')
+  React.useEffect(() => {
+    if(typeof window !== 'undefined'){
+      if(localStorage.getItem('textNotes')){
+        setTextNotes(localStorage.getItem('textNotes'))
+      }else{
+        localStorage.setItem('textNotes',TextNotes)
+      }
+    }
+  },[]);
+  const handleChange = (e) => {
+    setTextNotes(e.target.value)
+    localStorage.setItem('textNotes',e.target.value)
+  }
+  return (
+    <>
+      <Layout>
+      <TitlePage>Ghi chú văn bản</TitlePage>
+        <div className="  max-w-5xl p-3 border border-black bg-white m-auto">
+          <textarea  onChange={handleChange} spellCheck="true" value={TextNotes} placeholder="Nhập nội dung" rows={20}  className="w-full outline-none border border-black p-3 min-h-fit text-lg textMono"/>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default TextNotes;
